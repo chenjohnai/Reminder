dcab9621f08a6105ed250d0e4250759d2fcdbfc6
ccyykobe <chenjohnson92@gmail.com>
2014-08-11 00:23:33 +0800

    A transparent windows reminder, v 0.1
    easy to use, save logs to excel
    Yet. it remains version 0.1, lots of functions needed to be imporved.

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Office.Interop;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 win = new Window1();
+            win.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            readexcel();
+            killAllExcel();
+        }
+
+        public bool readexcel()
+        {
+             String path = AppDomain.CurrentDomain.BaseDirectory;
+             String filename = "deadlines.xlsx";
+             String file_dir = path + filename;
+             Microsoft.Office.Interop.Excel.Application excel_read = new Microsoft.Office.Interop.Excel.Application();
+             try
+             {
+                 Microsoft.Office.Interop.Excel.Workbook exbook = excel_read.Workbooks.Open(file_dir);
+                 if (exbook == null)
+                 {
+                     MessageBox.Show("read error");
+                 }
+                 Microsoft.Office.Interop.Excel.Worksheet exsheet = exbook.Sheets[1];
+                 int row_number = exbook.ActiveSheet.UsedRange.Rows.Count;
+                 int i = 0;
+                 while (i <= row_number)
+                 {
+                     String test  = exsheet.Cells.GetType().ToString();
+                     //if(exsheet.Cells.GetType().ToString() == double)
+                     //{
+                     //    DateTime time_scan = DateTime.FromOADate(double.Parse(exsheet.Cells[i, 1]));
+                     //}
+                     //else if(exsheet.Cells.GetType() == DateTime)
+                     //    DateTime timescan = DateTime.Parse(exsheet.Cells[i, 1].ToString())
+                     i++;
+                     //if (time_scan < DateTime.Now) continue;
+                     //else
+                     //{
+                     //    MessageBox.Show(time_scan.ToString());
+                     //    break;
+                     //}
+                 }
+                 TimeSpan cur_day = new TimeSpan(DateTime.Now.Ticks);
+                 i = 0 ;
+                 MessageBox.Show(exsheet.Cells[2, 1].Text);
+                 DateTime event1_dt =
+                     Convert.ToDateTime(exsheet.Cells[i + 1 , 1].Text);
+                 TimeSpan event1_ts = new TimeSpan(event1_dt.Ticks);
+                 event1.Text = exsheet.Cells[i + 1, 2].Text.ToString();
+//                 const String event1_text = (const) exsheet.Cells[ i + 1, 2];
+                 time1.Text = event1_ts.Subtract(cur_day).Duration().Days.ToString();
+                 DateTime event2_dt = Convert.ToDateTime(exsheet.Cells[i + 2, 1].Text);
+                 TimeSpan event2_ts = new TimeSpan(event2_dt.Ticks);
+                 event2.Text = exsheet.Cells[i + 2, 2].Text.ToString();
+                 time2.Text = event2_ts.Subtract(cur_day).Duration().Days.ToString();
+                 DateTime event3_dt = Convert.ToDateTime(exsheet.Cells[i + 3, 1].Text);
+                 TimeSpan event3_ts = new TimeSpan(event1_dt.Ticks);
+                 event3.Text = exsheet.Cells[i + 3, 2].Text.ToString();
+                 time3.Text = event3_ts.Subtract(cur_day).Duration().Days.ToString();
+                 DateTime event4_dt = Convert.ToDateTime(exsheet.Cells[i + 4, 1].Text);
+                 TimeSpan event4_ts = new TimeSpan(event1_dt.Ticks);
+                 event4.Text = exsheet.Cells[i + 4, 2].Text.ToString();
+                 time4.Text = event4_ts.Subtract(cur_day).Duration().Days.ToString();
+                 DateTime event5_dt = Convert.ToDateTime(exsheet.Cells[i + 5, 1].Text);
+                 TimeSpan event5_ts = new TimeSpan(event1_dt.Ticks);
+                 event5.Text = exsheet.Cells[i + 5, 2].Text.ToString();
+                 time5.Text = event5_ts.Subtract(cur_day).Duration().Days.ToString();
+                 return true;
+             }
+            catch(Exception e)
+             {
+                 MessageBox.Show(e.ToString());
+                 return false;
+             }
+            
+        }
+
+
+        public bool killAllExcel()
+        {
+            Microsoft.Office.Interop.Excel.Application excelApp = new Microsoft.Office.Interop.Excel.Application();
+            try
+            {
+                if (excelApp != null)
+                {
+                    excelApp.Quit();
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+                    //释放COM组件，其实就是将其引用计数减一
+
+                    foreach (System.Diagnostics.Process process in System.Diagnostics.Process.GetProcessesByName("EXCEL"))
+                    {
+                        if (process.CloseMainWindow() == false)
+                        {
+                            process.Kill();
+                        }
+                    }
+                    excelApp = null;
+                } return true;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                return false;
+            }
+            return true;
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e) 
+        {
+            if (e.LeftButton == MouseButtonState.Pressed) 
+            { 
+                DragMove(); 
+            } 
+        }
+
+    }
+}
---
+++ WpfApplication1/Window1.xaml.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Office.Interop;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        DateTime sel_date;
+        String eventname;
+        String eventdescription;
+        Boolean important;
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        protected void MonthlyCalendar_SelectedChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DatePicker datepicker1 = new DatePicker();
+            sel_date = (DateTime) date_picker.SelectedDate;
+            MessageBox.Show(sel_date.ToString());
+        }
+
+        protected void RichTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            eventname = Console.ReadLine();
+
+        }
+
+
+        public void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            eventdescription = Console.ReadLine();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            eventname = Event_Name.Text.ToString();
+            eventdescription = Event_Description.Text.ToString();
+            SaveDataToExcel(sel_date, eventname, eventdescription, important);
+            killAllExcel();
+            this.Close();
+        }
+
+        public static bool SaveDataToExcel(DateTime date, string event_name, string event_description, Boolean important_event)
+        {
+            Microsoft.Office.Interop.Excel.Application excel_write = new Microsoft.Office.Interop.Excel.Application();
+            try
+            {
+                String path = AppDomain.CurrentDomain.BaseDirectory;
+                String filename = "deadlines.xlsx";
+                String file_dir = path + filename;
+                excel_write.DisplayAlerts = false;
+                Microsoft.Office.Interop.Excel.Workbook exbook = excel_write.Workbooks.Open(file_dir);
+                if (exbook == null)
+                {
+                    MessageBox.Show("no find, create one");
+                    excel_write.Workbooks.Add();
+                    Microsoft.Office.Interop.Excel.Worksheet exsheet_tmp = exbook.Sheets[1];
+                    exsheet_tmp.SaveAs(file_dir);
+
+                }
+                Microsoft.Office.Interop.Excel.Worksheet exsheet = exbook.Sheets[1];
+                int row_num = exbook.ActiveSheet.UsedRange.Rows.Count;
+                //int row_num = exsheet.Rows.Count;
+                MessageBox.Show(row_num.ToString());
+                
+                exsheet.Cells[row_num + 1, 1] = date.ToShortDateString().ToString();
+                exsheet.Cells[row_num + 1, 2] = event_name;
+                exsheet.Cells[row_num + 1, 3] = event_description;
+
+                if (important_event == true)
+                {
+                    for(int i = 1 ; i<4; i++)
+                    {
+                        Microsoft.Office.Interop.Excel.Range im_rang = (Microsoft.Office.Interop.Excel.Range)exsheet.Cells[row_num + 1, i];
+                        im_rang.Interior.ColorIndex = 3;
+                       
+                    }
+                    MessageBox.Show("paint to red");
+                }
+
+////                if (important_event == true)
+//                {
+//                    Microsoft.Office.Interop.Excel.Range range = excel_write.get_Range("A1:A3, E1:G2");
+//                    range.Interior.ColorIndex = color;
+//                }
+
+                exsheet.SaveAs(file_dir);
+                MessageBox.Show("writing finished");
+
+                exbook.Close();
+
+
+                //excel_write.Application.Workbooks.Add(true);
+                //excel_write.Visible = true;
+                //if (excel_write == null) { MessageBox.Show("can't open excel"); return false; }
+                //excel_write.Workbooks.Add(true);
+                //int row = excel_write.Rows.Count;
+                //return true;
+                
+                return true;
+
+            }
+            catch (Exception e) {
+                MessageBox.Show(e.ToString());
+                return true;
+            }
+            
+        }
+
+        private void checkBox_Checked(object sender, RoutedEventArgs e)
+        {   
+            if((bool) checkBox.IsChecked)
+            {
+                important = true;
+                MessageBox.Show("this is important");
+            }
+        }
+
+        public bool killAllExcel()
+        {
+            Microsoft.Office.Interop.Excel.Application excelApp = new Microsoft.Office.Interop.Excel.Application();
+            try{
+                if(excelApp != null)
+                {
+                    excelApp.Quit();
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+                    //释放COM组件，其实就是将其引用计数减一
+                    
+                    foreach(System.Diagnostics.Process process in System.Diagnostics.Process.GetProcessesByName("EXCEL"))
+                    {
+                        if(process.CloseMainWindow() == false)
+                        {
+                            process.Kill();
+                        }
+                    }
+                    excelApp = null;
+                } return true;
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                return false;
+            }
+            return true;
+        
+        }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
